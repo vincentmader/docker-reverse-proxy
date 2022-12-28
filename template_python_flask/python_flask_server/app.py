67a7098fa3a08fce6ba0531b34c0a085ce8d3cb1
Vincent C. Mader <vincent.mader@icloud.com>
2022-12-28 22:16:42 +0100

    switched to port 5555

--- template_python_flask/python_flask_server/app.py
+++ template_python_flask/python_flask_server/app.py
@@ -10,4 +10,4 @@ def hello_world():
 
 if __name__ == "__main__":
     print("Hello, python3!")
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5555)
